a3ff72e0d0f027cbde01fe92c8a781bb479be9df
AlexanderManriquez <alexander.manriquez.s@gmail.com>
2024-11-24 17:02:55 -0300

    first commit with the functional project

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="app">
+    <h1>Tienda 32 Bits</h1>
+    <h3>Lista de Juegos</h3>
+    <table border="1" cellspacing="0" cellpadding="10">
+      <thead>
+        <tr>
+          <th>Código</th>
+          <th>Nombre</th>
+          <th>Stock</th>
+          <th>Precio</th>
+          <th>Acciones</th>
+        </tr>
+      </thead>
+      <tbody>
+        <Stock
+          v-for="juego in stock"
+          :key="juego.codigo"
+          :juego="juego"
+          @aumentar-stock="aumentarStock"
+          @disminuir-stock="disminuirStock"
+        />
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import { mapState, mapActions } from "vuex";
+import Stock from "./components/Stock.vue";
+
+export default {
+  name: "App",
+  components: {
+    Stock,
+  },
+  computed: {
+    ...mapState(["stock"]),
+  },
+  methods: {
+    ...mapActions(["incrementStock", "decrementStock"]),
+    aumentarStock(codigo) {
+      this.incrementStock(codigo);
+    },
+    disminuirStock(codigo) {
+      this.decrementStock(codigo);
+    },
+  },
+};
+</script>
+
+<style>
+table {
+  width: 100%;
+  text-align: center;
+}
+</style>
+
---
+++ src/components/Stock.vue
@@ -0,0 +1,46 @@
+<template>
+    <tr>
+      <td>{{ juego.codigo }}</td>
+      <td>{{ juego.nombre }}</td>
+      <td>{{ juego.stock }}</td>
+      <td>${{ juego.precio }}</td>
+      <td>
+        <button @click="aumentarStock">+</button>
+        <button @click="disminuirStock" :disabled="juego.stock === 0">-</button>
+      </td>
+    </tr>
+  </template>
+  
+  <script>
+  export default {
+    name: "Stock",
+    props: {
+      juego: {
+        type: Object,
+        required: true,
+      },
+    },
+    methods: {
+      aumentarStock() {
+        this.$emit("aumentar-stock", this.juego.codigo);
+      },
+      disminuirStock() {
+        this.$emit("disminuir-stock", this.juego.codigo);
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  button {
+    padding: 5px 10px;
+    margin: 0 5px;
+    cursor: pointer;
+  }
+  button:disabled {
+    cursor: not-allowed;
+    background-color: #ccc;
+  }
+  </style>
+  
+  
